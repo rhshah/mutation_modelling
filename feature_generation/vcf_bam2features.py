5d6e6ffcc47b6c6084501cb15fee2e1ad9a6c8f9
Ronak H Shah <rons.shah@gmail.com>
2017-07-24 20:22:25 -0400

    Test Truncation

--- feature_generation/vcf_bam2features.py
+++ feature_generation/vcf_bam2features.py
@@ -156,7 +156,7 @@ def generate_features(args):
 #         else:
 #             start = position -1
 #             end = position + 1
-        for rec in pysamstats.stat_variation_strand(bam_to_process, args.refFile, chrom=chromosome, start=position, end=position+1):
+        for rec in pysamstats.stat_variation_strand(bam_to_process, args.refFile, chrom=chromosome, start=position, end=position+1,truncate=True):
             rec = collections.OrderedDict(rec)
             rec = MyOrderedDict(rec)
             rec.prepend('Tumor_Seq_Allele1',alt)
